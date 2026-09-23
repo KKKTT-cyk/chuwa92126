c613ccabb2897ceeee0048b6b7cda1dc31531291
tiffiong <105686731+tiffiong@users.noreply.github.com>
2026-09-23 07:00:55 -0700

    Create Shape.java

---
+++ Coding/hw1/org/tiff/question11/Shape.java
@@ -0,0 +1,16 @@
+package org.tiff.question11;
+
+public abstract class Shape {
+    protected String color;
+
+    public Shape(String color) {
+        this.color = color;
+    }
+
+    public abstract double getArea();
+    public abstract double getPerimeter();
+
+    public String getColor() {
+        return this.color;
+    }
+}
